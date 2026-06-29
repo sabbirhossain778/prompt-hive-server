e2845453f9eff6547c1574dfa93a39ff4da12fbd
sabbirhossain778 <mshb5778@gmail.com>
2026-06-29 20:02:53 +0600

    update: latest server code deployment

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
 })
 
 
+const logger = (req, res, next) => {
+  console.log('logger middleware logged', req.params);
+  next();
+}
+
 
 const uri = process.env.MONGO_DB_URI;
 
@@ -39,6 +44,107 @@ async function run() {
     const planCollection = database.collection('plans');
     const subscriptionCollection = database.collection('subscriptions');
     const userCollection = database.collection('user');
+    const sessionCollection = database.collection('session');
+
+
+    // verification related
+    // const verifyToken = async (req, res, next) => {
+
+    //   const authHeader = req.headers?.authorization;
+    //   if (!authHeader) {
+    //     return res.status(401).send({ message: 'unauthorized access' })
+    //   }
+
+    //   const token = authHeader.split(' ')[1]
+    //   if (!token) {
+    //     return res.status(401).send({ message: 'unauthorized access' })
+    //   }
+
+    //   const query = { token: token }
+    //   const session = await sessionCollection.findOne(query);
+    //   console.log(session);
+
+    //   if (!session) {
+    //     return res.status(401).send({ message: 'unauthorized access' })
+    //   }
+
+    //   const userId = session.userId;
+    //   const userQuery = {
+    //     _id: userId
+    //   }
+
+    //   const user = await userCollection.findOne(userQuery);
+    //   if (!user) {
+    //     return res.status(401).send({ message: 'unauthorized access' })
+    //   }
+
+    //   // set data in the req object
+    //   req.user = user;
+    //   next();
+    // }
+    const verifyToken = async (req, res, next) => {
+      console.log(' varify called');
+      const authHeader = req.headers?.authorization;
+      console.log({ authHeader });
+
+      if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      console.log(1);
+
+
+      const token = authHeader.split(' ')[1]
+      console.log(2);
+
+
+      if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      console.log(3);
+      const query = { token: token }
+      const session = await sessionCollection.findOne(query);
+      console.log(4);
+      if (!session) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      console.log(5);
+      const userId = session.userId;
+
+
+      const userQuery = {
+        _id: userId
+      }
+
+      const user = await userCollection.findOne(userQuery);
+      if (!user) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      // set data in the req object
+      req.user = user;
+      next();
+    }
+
+    // verify admin
+    const verifyAdmin = async (req, res, next) => {
+      if (req.user?.role !== 'admin') {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next();
+    }
+    // verify creator
+    const verifyCreator = async (req, res, next) => {
+      if (req.user?.role !== 'creator') {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next();
+    }
+    // verify user
+    const verifyUser = async (req, res, next) => {
+      if (req.user?.role !== 'user') {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next();
+    }
 
 
     // plan
@@ -52,7 +158,7 @@ async function run() {
     });
 
     // subscription
-    app.post('/api/subscriptions', async (req, res) => {
+    app.post('/api/subscriptions', verifyToken, async (req, res) => {
       try {
         const { email, userId, planId, amount, currency, transactionId } = req.body;
 
@@ -83,38 +189,78 @@ async function run() {
       }
     });
 
-
-    app.get('/api/prompts', async (req, res) => {
-      const { creatorId, status, search, aiEngine, category, difficulty, sort } = req.query;
-
-      const query = {
-        status: 'approved',
-        visibility: { $regex: /^public$/i }
-      };
-
-      if (creatorId) {
-        query.creatorId = creatorId;
-        delete query.visibility;
+    // Get all payments for Admin Panel
+    app.get('/api/admin/payments', verifyToken, verifyAdmin, async (req, res) => {
+      try {
+        const payments = await subscriptionCollection.find().sort({ createdAt: -1 }).toArray();
+        res.send(payments);
+      } catch (error) {
+        res.status(500).send({ message: "Error fetching payments" });
       }
-
-      if (aiEngine && aiEngine !== 'All') query.aiTool = aiEngine;
-      if (category && category !== 'All') query.category = category;
-      if (difficulty && difficulty !== 'All') query.difficulty = difficulty;
-      if (search) query.promptTitle = { $regex: search, $options: 'i' };
-
-      let cursor = promptCollection.find(query);
-
-      // sort logic
-      if (sort === 'Most Popular') cursor = cursor.sort({ rating: -1 });
-      else if (sort === 'Most Copied') cursor = cursor.sort({ copies: -1 });
-      else cursor = cursor.sort({ createdAt: -1 });
-
-      const result = await cursor.toArray();
-      res.send(result);
     });
 
-    // logger saved prompts
-    app.get('/api/saved-prompts', async (req, res) => {
+
+    app.get('/api/prompts', async (req, res) => {
+      try {
+        const {
+          creatorId, search, aiEngine, category, difficulty, sort,
+          page = 1, limit = 9
+        } = req.query;
+
+        const pLimit = parseInt(limit);
+        const pPage = parseInt(page);
+        const skip = (pPage - 1) * pLimit;
+
+        // Match Stage
+        const matchStage = {
+          status: 'approved',
+          visibility: { $regex: /^public$/i }
+        };
+
+        if (creatorId) {
+          matchStage.creatorId = creatorId;
+          delete matchStage.visibility;
+        }
+        if (aiEngine && aiEngine !== 'All') matchStage.aiTool = aiEngine;
+        if (category && category !== 'All') matchStage.category = category;
+        if (difficulty && difficulty !== 'All') matchStage.difficulty = difficulty;
+        if (search) matchStage.promptTitle = { $regex: search, $options: 'i' };
+
+        // Pipeline create
+        const pipeline = [
+          { $match: matchStage },
+          {
+            $sort: sort === 'Most Popular' ? { rating: -1 } :
+              sort === 'Most Copied' ? { copies: -1 } : { createdAt: -1 }
+          },
+          {
+            $facet: { // data and total
+              "prompts": [{ $skip: skip }, { $limit: pLimit }],
+              "totalCount": [{ $count: "count" }]
+            }
+          }
+        ];
+
+        const result = await promptCollection.aggregate(pipeline).toArray();
+
+        // data processing
+        const prompts = result[0].prompts;
+        const totalCount = result[0].totalCount.length > 0 ? result[0].totalCount[0].count : 0;
+
+        res.send({
+          prompts: prompts,
+          totalPages: Math.ceil(totalCount / pLimit),
+          currentPage: pPage
+        });
+
+      } catch (error) {
+        console.error("Aggregation Error:", error);
+        res.status(500).send({ message: "Error fetching prompts" });
+      }
+    });
+
+
+    app.get('/api/saved-prompts', verifyToken, async (req, res) => {
       try {
         if (req.query.savedBy) {
           const userId = req.query.savedBy;
@@ -143,7 +289,7 @@ async function run() {
       res.json(trendingPrompts);
     });
 
-    app.get('/api/prompts/:id', async (req, res) => {
+    app.get('/api/prompts/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = {
         _id: new ObjectId(id)
@@ -152,8 +298,31 @@ async function run() {
       res.send(result)
     })
 
+    // get prompt by creator
+    app.get('/api/creator-prompts', verifyToken, async (req, res) => {
+      try {
+        const { creatorId, status } = req.query;
+        if (!creatorId) {
+          return res.status(400).send({ message: "Creator ID is required" });
+        }
+
+        const query = { creatorId: creatorId };
+        if (status && status !== 'all') {
+          query.status = status;
+        }
+
+        const prompts = await promptCollection.find(query).sort({ createdAt: -1 }).toArray();
+
+        res.send(prompts);
+
+      } catch (error) {
+        console.error("Error in /api/creator-prompts:", error);
+        res.status(500).send({ message: "Failed to fetch your prompts" });
+      }
+    });
+
     // Edit prompt
-    app.patch('/api/prompts/:id', async (req, res) => {
+    app.patch('/api/prompts/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const updatedData = req.body;
       const filter = { _id: new ObjectId(id) };
@@ -172,7 +341,7 @@ async function run() {
     });
 
     // Delete a prompt by ID
-    app.delete('/api/prompts/:id', async (req, res) => {
+    app.delete('/api/prompts/:id', verifyToken, async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
@@ -190,7 +359,7 @@ async function run() {
     });
 
     // Get total prompt count by creatorId ===========
-    app.get('/api/prompts/count/:creatorId', async (req, res) => {
+    app.get('/api/prompts/count/:creatorId', verifyToken, async (req, res) => {
       try {
         const creatorId = req.params.creatorId;
         const count = await promptCollection.countDocuments({ creatorId: creatorId });
@@ -200,7 +369,7 @@ async function run() {
       }
     });
 
-    app.post('/api/prompts', async (req, res) => {
+    app.post('/api/prompts', verifyToken, async (req, res) => {
       const prompt = req.body;
       const newPrompt = {
         ...prompt,
@@ -212,7 +381,7 @@ async function run() {
 
     // ========================
     // get all-users
-    app.get('/api/users', async (req, res) => {
+    app.get('/api/users', verifyToken, verifyAdmin, async (req, res) => {
       try {
         const users = await userCollection.find().toArray();
         res.send(users);
@@ -222,7 +391,7 @@ async function run() {
     });
 
     // Delete User
-    app.delete('/api/users/:id', async (req, res) => {
+    app.delete('/api/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
@@ -234,7 +403,7 @@ async function run() {
     });
 
     // Update User Role
-    app.patch('/api/users/:id/role', async (req, res) => {
+    app.patch('/api/users/:id/role', logger, verifyToken, verifyAdmin, async (req, res) => {
       try {
         const id = req.params.id;
         const { role } = req.body;
@@ -247,17 +416,32 @@ async function run() {
       }
     });
     //========================
-    
+
     // Get all prompts for Admin Moderation
-    app.get('/api/admin/all-prompts', async (req, res) => {
-        const result = await promptCollection.find().sort({ createdAt: -1 }).toArray();
-        res.send(result);
+    app.get('/api/admin/all-prompts', verifyToken, verifyAdmin, async (req, res) => {
+      try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = 10;
+        const skip = (page - 1) * limit;
+
+        const totalCount = await promptCollection.countDocuments();
+        const result = await promptCollection.find()
+          .sort({ createdAt: -1 })
+          .skip(skip)
+          .limit(limit)
+          .toArray();
+
+        res.send({ prompts: result, totalPages: Math.ceil(totalCount / limit) });
+      } catch (error) {
+        console.error("Backend Error:", error);
+        res.status(500).send({ message: "Server Error" });
+      }
     });
 
     // =======================
 
     // Bookmark APIs
-    app.get('/api/bookmarks/check/:promptId', async (req, res) => {
+    app.get('/api/bookmarks/check/:promptId', verifyToken, async (req, res) => {
       const promptId = req.params.promptId;
       const userId = req.query.userId;
 
@@ -272,14 +456,14 @@ async function run() {
     });
 
     // bookmarks count ===============
-    app.get('/api/bookmarks/count/:userId', async (req, res) => {
+    app.get('/api/bookmarks/count/:userId', verifyToken, async (req, res) => {
       const userId = req.params.userId;
       const count = await bookmarkCollection.countDocuments({ userId: userId });
       res.send({ count });
     });
 
     // Toggle Bookmark
-    app.post('/api/bookmarks/toggle', async (req, res) => {
+    app.post('/api/bookmarks/toggle', verifyToken, async (req, res) => {
       const { promptId, userId } = req.body;
       const query = { promptId, userId };
 
@@ -296,24 +480,53 @@ async function run() {
     });
 
 
-    // API: Submit a report
-    // app.post('/api/reports', async (req, res) => {
-    //   const { promptId, reporterId, reason, description } = req.body;
-    //   const newReport = {
-    //     promptId,
-    //     reporterId,
-    //     reason,
-    //     description,
-    //     status: 'pending',
-    //     createdAt: new Date()
-    //   };
 
-    //   const result = await reportCollection.insertOne(newReport);
-    //   res.send(result);
-    // });
+    // Get all reported prompts
+    app.get('/api/admin/reported-prompts', verifyToken, verifyAdmin, async (req, res) => {
+      const reports = await reportCollection.find().sort({ createdAt: -1 }).toArray();
+      res.send(reports);
+    });
+
+    // Admin Actions: Dismiss, Warn, Remove
+    app.patch('/api/admin/report/:id', verifyToken, verifyAdmin, async (req, res) => {
+      try {
+        const { action, promptId } = req.body;
+        const id = req.params.id;
+
+        if (action === 'remove') {
+          await promptCollection.deleteOne({ _id: new ObjectId(promptId) });
+        }
+
+        const result = await reportCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { status: 'resolved', adminAction: action } }
+        );
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: "Action failed" });
+      }
+    });
+
+    // API: Submit a report
+    app.post('/api/reports', verifyToken, async (req, res) => {
+      const { promptId, reporterId, reason, description } = req.body;
+      const newReport = {
+        promptId,
+        reporterId,
+        reason,
+        description,
+        status: 'pending',
+        createdAt: new Date()
+      };
+
+      const result = await reportCollection.insertOne(newReport);
+      res.send(result);
+    });
+
+
 
     // Copy account =====================
-    app.patch('/api/prompts/:id/copy', async (req, res) => {
+    app.patch('/api/prompts/:id/copy', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const update = { $inc: { copies: 1 } };
@@ -331,14 +544,14 @@ async function run() {
     });
 
     // Review API
-    app.get('/api/reviews/:promptId', async (req, res) => {
+    app.get('/api/reviews/:promptId', verifyToken, async (req, res) => {
       const { promptId } = req.params;
       const reviews = await reviewCollection.find({ promptId }).sort({ createdAt: -1 }).toArray();
       res.send(reviews);
     });
 
     // Get all reviews posted by a specific user
-    app.get('/api/reviews/user/:userId', async (req, res) => {
+    app.get('/api/reviews/user/:userId', verifyToken, async (req, res) => {
       try {
         const userId = req.params.userId;
 
@@ -369,7 +582,7 @@ async function run() {
       }
     });
 
-    app.post('/api/reviews', async (req, res) => {
+    app.post('/api/reviews', verifyToken, async (req, res) => {
       console.log("Received Data:", req.body);
       const { promptId, userId, rating, reviewText, userName, userImage, role } = req.body;
       const newReview = {
@@ -387,7 +600,7 @@ async function run() {
     });
 
     // prompt stats
-    app.get('/api/prompts/stats/:creatorId', async (req, res) => {
+    app.get('/api/prompts/stats/:creatorId', verifyToken, async (req, res) => {
       const prompts = await promptCollection.find({ creatorId: req.params.creatorId }).toArray();
       const stats = prompts.map(p => ({
         name: p.promptTitle.slice(0, 10),
@@ -397,10 +610,10 @@ async function run() {
       res.json(stats);
     });
 
-// not for client side ,,give me git commit for server side
+    // not for client side ,,give me git commit for server side
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
